8d38d5ddf434cbdf87328b8689fbf80843617b50
Maxime LAMAISON <lamais_m@modulotech.fr>
2019-01-30 11:09:53 +0100

    pacth/aBitOfStyle

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="col-3 mb-1">
+  <div class="col-3 mb-4 d-flex align-items-stretch">
     <div class="card">
       <img class="card-img-top" :src="product.img" alt="Card image cap">
-      <div class="card-body">
+      <div class="card-body d-flex flex-column justify-content-between">
         <h5 class="card-title">
           {{product.name}}
         </h5>
         <p class="card-text">
           {{product.description}}
         </p>
-        <button class="btn btn-primary" data-cy-addToCart @click="addToCart()">Ajouter au panier</button>
+        <button class="align-self-end btn btn-primary btn-block" data-cy-addToCart @click="addToCart()">Ajouter au panier</button>
       </div>
     </div>
   </div>
